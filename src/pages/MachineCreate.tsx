2ba621d8f08ec600c985154313db381f239d8539
Flavio Montoril <flavio.montorilll@gmail.com>
2025-06-02 02:41:12 -0300

    refactor

--- src/pages/MachineCreate.tsx
+++ src/pages/MachineCreate.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigate } from "react-router-dom";
-
-
 import { useApi } from "../services/useApi";
 import { toast } from "sonner";
 import { useTaskStore } from "../store/MachineStore";
